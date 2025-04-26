65cbccb5f7889cbf95a09114ffea471856231117
Caleb Martin <calebmartin2020@gmail.com>
2025-04-26 03:35:07 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+import { about } from "./about";
+import { contact } from "./contact";
+import { menuItems } from "./menu-items";
+
+
+console.log(about);
+console.log(contact);
+console.log(menuItems);
