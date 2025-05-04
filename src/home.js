d086ab4a237c93e5ef0f171f2257a9ab76028048
Caleb Martin <calebmartin2020@gmail.com>
2025-05-03 20:11:16 -0500

    feat: added home and menu content and styling

--- src/home.js
+++ src/home.js
@@ -2,7 +2,7 @@ export const home = () => {
   const content = document.createElement("div");
   content.classList.add("home");
   content.innerHTML = `
-    <p class="home-title">The food restaurant</p>
-    <p class="home-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>`;
+    <p class="home-title">The Berry Bar</p>
+    <p class="home-description">- If a berry exists, we have a smoothy for it!</p>`;
   return content;
 };
--- src/menu-items.js
+++ src/menu-items.js
@@ -2,7 +2,37 @@ export const menuItems = () => {
   const content = document.createElement("div");
   content.classList.add("menu");
   content.innerHTML = `
-    <p class="menu-title">Menu</p>
-    <p class="menu-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>`;
+    <p class="menu-title">Smoothies On Tap</p>
+    <div id="menu-container">
+      <div class="menu-item">
+        <p class="menu-item-name">Berry Blast</p>
+        <p class="menu-item-description">A blend of strawberries, blueberries, and raspberries.</p>
+        <div class="berry-smoothie"></div>
+        <div class="glass-stem"></div>
+        <div class="glass-base"></div>
+      </div>
+      <div class="menu-item">
+        <p class="menu-item-name">Tropical Paradise</p>
+        <p class="menu-item-description">Mango, pineapple, and coconut for a taste of the tropics.</p>
+        <div class="tropical-smoothie"></div>
+        <div class="glass-stem"></div>
+        <div class="glass-base"></div>
+      </div>
+      <div class="menu-item">
+        <p class="menu-item-name">Green Goddess</p>
+        <p class="menu-item-description">Spinach, kale, and green apple for a healthy boost.</p>
+        <div class="green-smoothie"></div>
+        <div class="glass-stem"></div>
+        <div class="glass-base"></div>
+      </div>
+      <div class="menu-item">
+        <p class="menu-item-name">Chocolate Delight</p>
+        <p class="menu-item-description">Chocolate, banana, and almond milk for a sweet treat.</p>
+        <div class="chocolate-smoothie"></div>
+        <div class="glass-stem"></div>
+        <div class="glass-base"></div>
+      </div>
+    </div>`;
+
   return content;
 }
